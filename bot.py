0163bc4d4f84264aaea838261b5b20396dd98ae7
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-03-24 11:48:49 -0400

    Commit #2 - Setting up Project, Basic Pattern Matching

--- bot.py
+++ bot.py
@@ -1,28 +1,34 @@
 
-
-def load_data():
-    return None 
+from data.load_data import load_json_data
 
 def understand(utterance):
-    return None 
+    for i, question in enumerate(intents): 
+        if question == utterance:
+            return i
+    return -1
 
 def generate(intent):
-    return None
+    if intent == -1:
+        return "Sorry, I don't know the answer to that!"
+    return responses[intent]
+
+intents, responses = load_json_data() 
 
 def main():
-    print("Hello! I know stuff about chat bots. When you're done talking, just say 'goodbye'.")
+    print("Welcome to Brendan's Bot!")
     print()
-    utterance = ""
+    print("To begin interacting with the bot, greet it.")
+    print("For example, you can say 'hi', 'hello', or similar greetings!")
+    print("To stop interacting with the bot, say goodbye.")
+    print("For example, you can say 'goodbye', 'bye', or similar goodbyes!")
+
     while True:
-        utterance = input(">>> ")
-        if utterance == "goodbye":
-            break;
+        utterance = input(">>>").lower()
+
         intent = understand(utterance)
         response = generate(intent)
         print(response)
         print()
-
-    print("Nice talking to you!")
 
 
 if __name__ == "__main__":
---
+++ data/load_data.py
@@ -0,0 +1,20 @@
+
+import json 
+
+
+def load_json_data():
+    """
+    Reads questions and answers, and returns two parallel key value lists.
+    """
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file) 
+            print("Data Loaded Successfully!") 
+        return data["questions"], data["answers"]
+    except FileNotFoundError:
+        print("Error: The data file is missing!")
+    except json.JSONDecodeError: 
+        print("Error: There was an issue decoding the JSON data!")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return [], []
