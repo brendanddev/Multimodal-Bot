325ce04bf9046d297001d32a62efb1b84e648bfb
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-03-25 11:14:30 -0400

    Commit #11 - Implementing Pattern Matching and NLP with Fuzzy

--- intent.py
+++ intent.py
@@ -1,6 +1,6 @@
+
 import regex as re
 from fuzzywuzzy import fuzz
-
 
 def clean_utterance(utterance):
     clean = re.sub(r'[^\w\s]', '', utterance) 
@@ -21,11 +21,28 @@ def fuzzy_match(utterance, intents):
 
     return best_match, best_score
 
+def fuzzy_regex_match(utterance, regex_patterns):
+    best_match = None 
+    best_score = -1
+
+    for pattern in regex_patterns:
+        score = fuzz.partial_ratio(utterance, pattern)
+        if score > best_score:
+            best_score = score
+            best_match = pattern 
+    return best_match, best_score
+
+
 def understand(utterance, intents, regex_patterns):
     cleaned_utterance = clean_utterance(utterance)
+
+    match, score = fuzzy_regex_match(cleaned_utterance, regex_patterns)
+    if match and score > 60:
+        return regex_patterns.index(match)
+
     for i, pattern in enumerate(regex_patterns):
-        if re.match(pattern, cleaned_utterance, re.IGNORECASE):  
-            return i
+        if re.match(pattern, cleaned_utterance, re.IGNORECASE):
+            return i 
     
     match, score = fuzzy_match(cleaned_utterance, intents)
     if match and score > 60:
