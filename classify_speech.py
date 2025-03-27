aa91ec9b32e847d319c4cd8646f36e091d869a4a
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-03-27 15:01:09 -0400

    Commit #18 - Adding more robust Entity Recognition

--- classify_speech.py
+++ classify_speech.py
@@ -65,7 +65,7 @@ def extract_entities(utterance):
         elif ent.label_ == "LOCATION":
             entity_responses.append(f"{ent.text} sounds like a great place! I have never been!")
         elif ent.label_ == "MONEY":
-            entity_responses.append(f"Im not familiar with currencies like {ent.text}!")
+            entity_responses.append(f"I'm not familiar with currencies like {ent.text}!")
         elif ent.label_ == "DATE":
             entity_responses.append(f"I'm not sure when {ent.text} is...")
         elif ent.label_ == "TIME":
@@ -80,6 +80,21 @@ def extract_entities(utterance):
             entity_responses.append(f"I don't know how to speak {ent.text}!")
         elif ent.label_ == "FAC":
             entity_responses.append(f"{ent.text} sounds cool!")
+
+    for chunk in doc.noun_chunks:
+        if chunk.root.pos_ not in ['PRON', 'DET']:
+            entity_responses.append(f"Sorry, I don't know anything about {chunk.text}!")
+
+    for token in doc:
+        if token.is_alpha and not token.is_stop:
+            if token.pos_ == "VERB":
+                entity_responses.append(f"Sorry, I don't know how to {token.lemma_}, could you try rephrasing?")
+            elif token.pos_ == "NOUN":
+                entity_responses.append(f"I don't know anything about {token.text}, sorry!")
+            elif token.pos_ == "ADJ":
+                entity_responses.append(f"I do not know anything about {token.text}! Try to clarify.")
+            elif token.pos_ == "ADV":
+                entity_responses.append(f"Sorry, I don't know what you mean by {token.text}!")
 
     return entity_responses
 
