027f4a4280ee73de8c8a9fab225f1d041357813c
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-03-28 12:35:30 -0400

    Commit #28 - Basic Setup of the Discord Bot

--- discord.py
+++ discord.py
@@ -1,3 +1,13 @@
 
 import discord
 from discord.ext import commands 
+
+# Initializes the bots intents and access to content
+intents = discord.Intents.default()
+intents.message_content = True 
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}!')
