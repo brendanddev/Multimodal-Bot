22f87fcc95873362b84581e2f40fef42cd4e33eb
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-03-31 14:25:16 -0400

    Commit #3 - Basic Bot Interaction Structure

---
+++ bot.py
@@ -0,0 +1,36 @@
+
+from data.load_data import load_json_data
+from utils.process_text import clean_utterance
+
+def understand(utterance):
+    cleaned_utterance = clean_utterance(utterance)
+    for i, question in enumerate(intents): 
+        if clean_utterance(question) == cleaned_utterance:
+            return i
+    return -1
+
+def generate(intent):
+    if intent == -1:
+        return "Sorry, I don't know the answer to that!"
+    return responses[intent]
+
+intents, responses = load_json_data() 
+
+def main():
+    print("Welcome to Brendan's FAQ Bot!")
+    print()
+    print("To begin interacting with the bot, greet it.")
+    print("For example, you can say 'hi', 'hello', or similar greetings!")
+    print("To stop interacting with the bot, say goodbye.")
+    print("For example, you can say 'goodbye', 'bye', or similar goodbyes!")
+
+    while True:
+        utterance = input(">>>").lower()
+        intent = understand(utterance) 
+        response = generate(intent)
+        print(response)
+        print()
+
+
+if __name__ == "__main__":
+    main()
