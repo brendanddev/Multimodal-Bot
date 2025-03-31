237ff1f15d10304d484fc60a2a18714dd93a9ca9
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-03-31 14:24:30 -0400

    Commit #2 - Providing Basic Data to the Bot

---
+++ data/load_data.py
@@ -0,0 +1,17 @@
+
+import json 
+
+
+def load_json_data():
+    try:
+        with open("data/data.json", "r") as file:
+            data = json.load(file) 
+            print("Data Loaded Successfully!") 
+        return data["questions"], data["answers"]
+    except FileNotFoundError:
+        print("Error: The data file is missing!")
+    except json.JSONDecodeError: 
+        print("Error: There was an issue decoding the JSON data!")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return [], []
