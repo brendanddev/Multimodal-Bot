550e29c8693554d24fb83cc35f8cfa38640b7d8b
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-04-12 14:09:17 -0400

    Begin Linguistic Extraction

---
+++ utils/linguistic_extraction.py
@@ -0,0 +1,11 @@
+"""
+linguistic_extraction.py
+
+Brendan Dileo, April 2025
+"""
+
+import spacy 
+nlp = spacy.load("en_core_web_md")
+
+string = "I hear GitLab is great for aliens working on a project!"
+
