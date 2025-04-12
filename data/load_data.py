8ca3438ffaf3ef4a28de6c1663e65abc55d3f015
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-04-12 11:29:14 -0400

    Add Basic Load Data Logic

--- data/load_data.py
+++ data/load_data.py
@@ -1,2 +1,16 @@
-
 # load_data.py
+import json 
+
+def load_json_data():
+    try:
+        with open("data/data.json", "r") as file:
+            data = json.load(file) 
+            print("Data Loaded Successfully!") 
+        return data["questions"], data["answers"]
+    except FileNotFoundError:
+        print("Error: The data file is missing!")
+    except json.JSONDecodeError: 
+        print("Error: There was an issue decoding the JSON data!")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return [], []
