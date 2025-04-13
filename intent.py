f718c1147993fee291be53051b991cd6da29b22a
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-04-13 12:05:18 -0400

    Add Command Linguistic Pattern Matching

--- intent.py
+++ intent.py
@@ -10,12 +10,15 @@ Brendan Dileo, April 2025
 from utils.process_text import clean_utterance
 from utils.pattern_matching import heuristic_match
 from utils.linguistic_extraction import extract_linguistics
+from utils.linguistic_patterns import match_patterns
 
 def understand(utterance, intents, regex_patterns):
     """ Processes an utterance to determine if an intent matches"""
     cleaned_utterance = clean_utterance(utterance)
     ling_info = extract_linguistics(utterance)
+    ling_patterns = match_patterns(utterance)
     print(ling_info)
+    print(ling_patterns)
     intent = heuristic_match(cleaned_utterance, intents, regex_patterns)
     return intent 
 
--- utils/linguistic_patterns.py
+++ utils/linguistic_patterns.py
@@ -13,7 +13,8 @@ def match_patterns(utterance):
     # Initialize matcher
     matcher = Matcher(nlp.vocab)
 
-    pattern = [
+    # Basic question matcher pattern
+    question_pattern = [
         # Common question words
         {"LOWER": {"in": ["who", "what", "were", "where", "when", "why", "how"]}}, 
         # Token must be aux verb and occur one or more times
@@ -22,8 +23,17 @@ def match_patterns(utterance):
         {"POS": "PRON", "OP": "?"}  
     ]
 
-    # Add the pattern to the matcher
-    matcher.add("QUESTION_PATTERN", [pattern])
+    # Basic command matcher pattern
+    command_pattern = [
+        # Starts with a verb
+        {"POS": "VERB"},
+        # Optional noun following verb
+        {"POS": "NOUN", "OP": "?"}
+    ]
+
+    # Add the patterns to the matcher
+    matcher.add("QUESTION_PATTERN", [question_pattern])
+    matcher.add("COMMAND_PATTERN", [command_pattern])
 
     # Process the users utterance into a doc object
     doc = nlp(utterance)
@@ -32,14 +42,10 @@ def match_patterns(utterance):
     matches = matcher(doc)
 
     if matches:
-        print("\n--- Question Pattern Matches Found ---")
+        print("\n--- Pattern Matches Found ---")
         for match_id, start, end in matches:
             pattern_id = nlp.vocab.strings[match_id]
             span = doc[start:end]  # The matched span
             print(f"Match ID: {pattern_id}, Matched phrase: {span.text}")
     else:
         print("\nNo question matches found.")
-
-if __name__ == "__main__":
-    user_input = input("Enter a sentence for question pattern matching: ")
-    match_patterns(user_input)
