8c8d14e132b51341098e348e0edf0291a9f2c8bb
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-04-13 11:02:06 -0400

    Tokens and POS Identification

--- utils/linguistic_extraction.py
+++ utils/linguistic_extraction.py
@@ -6,6 +6,19 @@ Brendan Dileo, April 2025
 
 import spacy 
 nlp = spacy.load("en_core_web_md")
-
 string = "I hear GitLab is great for aliens working on a project!"
 
+def extract_linguistics(utterance):
+    """ Analyzes and prints the linguistic data from the users utterance """
+    # Process the users utterance
+    doc = nlp(utterance)
+
+    # Tokens and Parts of speech
+    print(" --- Tokens and POS --- ")
+    for token in doc:
+        print(f"{token.text:<15} POS: {token.pos_}")
+
+
+if __name__ == "__main__":
+    user_input = input("Please enter a sentence for linguistic analysis: ")
+    extract_linguistics(user_input)
