fdfec47d00ee4f2f6b5d3f9be1defb06a57ed246
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-04-17 16:22:06 -0400

    Create Basic Graph

--- utils/graphs.py
+++ utils/graphs.py
@@ -5,6 +5,19 @@ Brendan Dileo, April 2025
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
+def generate_graph(start, end, points):
+    # x vals between start and end with specified num of points
+    x = np.linspace(start, end, points)
+    # Linear to test
+    y = x  
 
-def generate_graph():
+    # Creates a figure and plot graph
+    plt.figure(figsize=(6, 4))
+    plt.plot(x, y, label='y = x') 
+    plt.title('Basic Graph: y = x')  
+    plt.xlabel('x')  
+    plt.ylabel('y')  
+    plt.grid(True)  
+    plt.legend()
