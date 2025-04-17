d7cf807ee76eac1a24a2e5711db8716332cd48c9
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-04-17 17:07:02 -0400

    Expand on Current Graphing with Time Complexity Graph Option

--- utils/graphs.py
+++ utils/graphs.py
@@ -4,20 +4,20 @@ graphs.py
 Brendan Dileo, April 2025
 """
 
+import io
+import math
 import numpy as np
 import matplotlib.pyplot as plt
-import io
 
 def generate_graph(start, end, points):
     # x vals between start and end with specified num of points
     x = np.linspace(start, end, points)
-    # Linear to test
-    y = x  
+    y = x**2
 
     # Creates a figure and plot graph
     plt.figure(figsize=(6, 4))
-    plt.plot(x, y, label='y = x')
-    plt.title('Basic Graph: y = x')
+    plt.plot(x, y, label='y = x^2')
+    plt.title('Graph: y = x^2')
     plt.xlabel('x')
     plt.ylabel('y')  
     plt.grid(True)
@@ -29,10 +29,11 @@ def generate_graph(start, end, points):
     buf.seek(0)
     return buf
 
-# Generate graph and save to RAM temporarily?
+def generate_time_complexity_graph():
+
+# Generate graph and save to RAM temporarily
 buffer = generate_graph(10, 100, 10)
 
-# ignore graph already
 # Save to a file for local testing
 with open('graph.png', 'wb') as file:
     file.write(buffer.read())
