242767e6232e258376be2c82e34e2be835298b84
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-04-24 14:07:34 -0400

    Add Basic Functions for Interacting with Backend

--- utils/xp_handler.py
+++ utils/xp_handler.py
@@ -1,11 +1,31 @@
 """
 xp_handler.py
 
+Helper file to interact with the backend API server.
+
 Brendan Dileo, April 2025
 """
 
 import aiohttp 
-from levels import Level
+from utils.levels import Level
 
 API_URL = 'http://localhost:3001'
 
+# Fetches the users rank based on amount of xp
+def fetch_rank(xp: float):
+    # Sort levels by xp highest to lowest
+    sorted_levels = sorted(Level, key=lambda l: l.value, reverse=True)
+    for level in sorted_levels:
+        if xp >= level.value:
+            return level.name 
+    return Level.Noob.name
+
+# Fetches a user from the backend api
+async def fetch_user(username):
+    # Creates a session for http requests
+    async with aiohttp.ClientSession() as session:
+        # Makes a GET request
+        async with session.get(f'{API_URL}/user/{username}') as response:
+            if response.status == 200:
+                return await response.json()
+            return None
