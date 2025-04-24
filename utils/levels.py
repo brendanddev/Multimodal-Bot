7f11313e2f20cc4dd071dad3522271353e3c728c
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-04-24 12:25:44 -0400

    Add Function for Getting the Users Level

--- utils/levels.py
+++ utils/levels.py
@@ -6,5 +6,24 @@ Brendan Dileo, April 2025
 
 from enum import Enum
 
+# Level enum for tracking xp thresholds
+class Level(Enum):
+    Noob = 0
+    Middleman = 50
+    Veteran = 150
+    Elite = 300
+    Legend = 750
 
+    # Retreives the users current level
+    @classmethod
+    def get_level(cls, xp: int) -> 'Level':
+        levels = sorted(cls, key=lambda l: l.value)
+        current_level = levels[0]
+        for level in levels:
+            if xp >= level.value:
+                current_level = level
+            else:
+                break 
+
+        return current_level
 
