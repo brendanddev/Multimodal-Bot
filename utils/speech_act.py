ca4751bf1eba097175759d68feb0a8b12cede076
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-04-30 13:57:48 -0400

    Add Basic Speech Act Classification

--- utils/speech_act.py
+++ utils/speech_act.py
@@ -21,3 +21,18 @@ command_patterns = [
     [{"POS": "VERB", "LEMMA": "read"}],  
     [{"LOWER": "please"}, {"POS": "VERB"}]
 ]
+
+matcher.add("QUESTION", question_patterns)
+matcher.add("COMMAND", command_patterns)
+
+def classify_speech_act(utterance):
+    doc = nlp(utterance)
+    matches = matcher(doc)
+
+    # Check whether utterance is a question, statement, etc
+    for match_id, start, end in matches:
+        if nlp.vocab.strings[match_id] == "QUESTION":
+            return "question"
+        elif nlp.vocab.strings[match_id] == "COMMAND":
+            return "command"
+    return "statement"
